6887f0dc71c7846570c4f2472b3cb1dc23bfc598
Kangfu00 <kemmatat323@gmail.com>
2025-10-18 14:08:15 +0700

    good

---
+++ src/app/components/header.js
@@ -0,0 +1,14 @@
+let mycss = {
+    fontsize:100,
+    color: 'blue'
+}
+
+export default function Header(){
+  return (
+    <>
+    <center>
+      <p style={mycss}>これがヘエト</p>
+    </center>
+    </>
+  )
+}
